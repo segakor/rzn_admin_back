725dd1da644cf8ab3de8204e0110008d464ce1c7
Sergey Kornienko <SDKornienko@inno.local>
2024-09-08 15:20:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,15 @@ const PORT = 5001;
 const app = express();
 
 /* app.use(cors()); */
-app.use(cors({ credentials: true, origin: "https://test.ryazantourism.ru" })); 
+/* app.use(cors({ credentials: true, origin: "https://test.ryazantourism.ru" }));  */
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.json());
 
 
